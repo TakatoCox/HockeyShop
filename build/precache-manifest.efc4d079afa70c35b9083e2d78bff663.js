8d0b210053bbb76b4fd036aa2f5c0ae3ebed2999
Takato Cox <takatohockey@gmail.com>
2020-07-08 13:48:49 -0700

    hockey

---
+++ build/precache-manifest.efc4d079afa70c35b9083e2d78bff663.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdaa2971973cd786118f58f25581a1af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d095ecc88f4bce7c6eac",
+    "url": "./static/css/main.b138e65d.chunk.css"
+  },
+  {
+    "revision": "974792a580e284d28b16",
+    "url": "./static/js/2.358dabd7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.358dabd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d095ecc88f4bce7c6eac",
+    "url": "./static/js/main.c2f1570c.chunk.js"
+  },
+  {
+    "revision": "898a8113551618006355",
+    "url": "./static/js/runtime-main.65c8e657.js"
+  },
+  {
+    "revision": "78e501c596bbb050243fffdf80dc6d00",
+    "url": "./static/media/bauer.78e501c5.png"
+  },
+  {
+    "revision": "b2f579c9e676900b8237dcf046725b2c",
+    "url": "./static/media/bauerHelmet.b2f579c9.jpg"
+  },
+  {
+    "revision": "ea3462995331a1e78736adfe75d9982a",
+    "url": "./static/media/bauerSkates.ea346299.png"
+  },
+  {
+    "revision": "7a15f68483cf9d85015dbb08378f6433",
+    "url": "./static/media/bauerStick.7a15f684.png"
+  },
+  {
+    "revision": "5a2d793274d500c9320cd73186728fed",
+    "url": "./static/media/ccm.5a2d7932.png"
+  },
+  {
+    "revision": "2b14279cb7f3e0c95f28657881e071bf",
+    "url": "./static/media/ccmHelmet.2b14279c.jpg"
+  },
+  {
+    "revision": "8d8429f1e8f8b278f25b8e2ed3df3878",
+    "url": "./static/media/ccmSkates.8d8429f1.jpg"
+  },
+  {
+    "revision": "9294f28b57111926356306b1a6383f0d",
+    "url": "./static/media/ccmStick.9294f28b.jpg"
+  },
+  {
+    "revision": "aa4ab9d38a5a5cce68241bffcbc6b06a",
+    "url": "./static/media/elbow.aa4ab9d3.jpg"
+  },
+  {
+    "revision": "f14f636fc23aede2740492558bb37cb0",
+    "url": "./static/media/gloves.f14f636f.jpg"
+  },
+  {
+    "revision": "adf988cdd7d3312c57ee556fee79b7cd",
+    "url": "./static/media/grafSkates.adf988cd.jpg"
+  },
+  {
+    "revision": "3138c32941852351ef8e38707350167d",
+    "url": "./static/media/grafStick.3138c329.jpg"
+  },
+  {
+    "revision": "3b001c7d617e57012dd1ae9856c1a60e",
+    "url": "./static/media/ice-hockey.3b001c7d.png"
+  },
+  {
+    "revision": "a6c9a46466acd04b9e25a6e0b41ada3c",
+    "url": "./static/media/ice.a6c9a464.jpg"
+  },
+  {
+    "revision": "675f848873481d587e3ff4aa2e598d94",
+    "url": "./static/media/jock.675f8488.jpg"
+  },
+  {
+    "revision": "5c82b4205a9028fba52be540a0be738c",
+    "url": "./static/media/longShirt.5c82b420.jpg"
+  },
+  {
+    "revision": "f70e7507f4fb3b186bb5397585269204",
+    "url": "./static/media/pants.f70e7507.jpg"
+  },
+  {
+    "revision": "957a87cd35d016a47cd8e24a070105e3",
+    "url": "./static/media/sherwood.957a87cd.png"
+  },
+  {
+    "revision": "cd3398e8f7d4203b88328fab2a1262d5",
+    "url": "./static/media/sherwoodStick.cd3398e8.jpg"
+  },
+  {
+    "revision": "88012de4ed874b05076ef77764ba8c61",
+    "url": "./static/media/shin.88012de4.jpg"
+  },
+  {
+    "revision": "327d7d625ad949393595d7ece49cc538",
+    "url": "./static/media/shortShirt.327d7d62.jpg"
+  },
+  {
+    "revision": "cb03039cee30772bfc943964f6df5c6c",
+    "url": "./static/media/shoulder.cb03039c.jpg"
+  },
+  {
+    "revision": "885594c03423eb5f8937e182b6265d9f",
+    "url": "./static/media/socks.885594c0.jpg"
+  },
+  {
+    "revision": "c54c9e5c63f29ef0de83a77779589b9a",
+    "url": "./static/media/trueSkates.c54c9e5c.jpg"
+  },
+  {
+    "revision": "f3923835045428e522292907973e1681",
+    "url": "./static/media/trueStick.f3923835.jpg"
+  },
+  {
+    "revision": "e749bd67d7a34f0b6708d9de55364567",
+    "url": "./static/media/warrior.e749bd67.png"
+  },
+  {
+    "revision": "2a0c6f54e35402cb058f2fb3f1aa5f7e",
+    "url": "./static/media/warriorHelmet.2a0c6f54.jpg"
+  },
+  {
+    "revision": "264ba53a32c17e73665aaf46e4394164",
+    "url": "./static/media/warriorStick.264ba53a.png"
+  }
+]);
